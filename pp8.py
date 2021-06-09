7dd5564c4a0cc642cc7b3d0c20e52a164c7a7836
AntonioAlvesSU <67467354+AntonioAlvesSU@users.noreply.github.com>
2021-06-09 14:59:41 -0300

    Atualizado em 09062021

---
+++ pp8.py
@@ -0,0 +1,50 @@
+"""
+PEP8 - Python Enhancement Proposal
+São propostas de melhorias para a linguagem Python
+The Zen of Python
+import this -comando par ester eggo
+The Zen of Python, by Tim Peters
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+################################################################################
+A idéia da PEP8 para que possamos escrever os códigos de forma Phythonica.
+[1] - Utilize o Camel case para nomes de classes;
+[2] - Dê dois espaços em linha por código;
+[3] - Utlçize nomes em minusculos, separados por unederline para funções ou variaveis;
+
+"""
+
+class Calcualdora:
+    pass
+
+class CalcualdoraCientifica:
+    pass
+
+def soma():
+    pass
+
+def soma_dois():
+    pass
+
+
+
+
+
+
